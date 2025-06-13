b15612015065b656e479597597686bfd25006d27
Enemali01 <simonnugwa7@gmail.com>
2025-06-13 23:03:11 +0100

    updates

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: process.env.BASE_URL === 'production',
+        secure: true,
         sameSite: 'Strict',
         maxAge: 5 * 24 * 60 * 60 * 1000 // 5 days
       })
